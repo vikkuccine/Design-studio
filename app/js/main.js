b7e9a0d8c3e515f096947524349387f2d1de98df
vikkuccine <kamenkova09@gmail.com>
2023-09-10 23:58:22 +0300

    progress

--- app/js/main.js
+++ app/js/main.js
@@ -1,34 +1,37 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-  const element = document.querySelector('.js-choice');
-  const choices = new Choices(element, {
-    searchEnabled: false,
-    choices: [
-      {
-        value: 'UA',
-        label: 'UA',
-        selected: window.location.pathname === '/index-ua.html',
-      },
-      {
-        value: 'EN',
-        label: 'EN',
-        selected: window.location.pathname === '/index.html' || window.location.pathname === '/' ,
-      },
-    ]
-  });
+  const elements = document.querySelectorAll('.js-choice');
+  elements.forEach((element) => {
+    const choices = new Choices(element, {
+      searchEnabled: false,
+      choices: [
+        {
+          value: 'UA',
+          label: 'UA',
+          selected: window.location.pathname === '/index-ua.html',
+        },
+        {
+          value: 'EN',
+          label: 'EN',
+          selected: window.location.pathname === '/index.html' || window.location.pathname === '/',
+        },
+      ]
+    });
 
-  element.addEventListener(
-    'choice',
-    function (event) {
-      if (event.detail.choice.value === 'UA') {
-        window.location.href = "index-ua.html";
-      } else {
-        window.location.href = "/";
-      }
+    element.addEventListener(
+      'choice',
+      function (event) {
+        if (event.detail.choice.value === 'UA') {
+          window.location.href = "index-ua.html";
+        } else {
+          window.location.href = "/";
+        }
 
-    },
-    false,
-  );
+      },
+      false,
+    );
+  })
+
 
   if (document.querySelector('.gallery__inner')) {
     var mixer = mixitup('.gallery__inner', {
@@ -66,6 +69,39 @@ document.addEventListener('DOMContentLoaded', function () {
   Fancybox.bind('[data-fancybox="gallery-5"]', {
     // Your custom options
   });
+
+
+  let menuBtn = document.querySelector('.burger-menu');
+  let menu = document.querySelector('.burger-text');
+  let body = document.querySelector('body')
+
+  menuBtn.addEventListener('click', function () {
+    menuBtn.classList.toggle('burger-menu--open');
+    menu.classList.toggle('burger-text--open');
+    body.classList.toggle('overflow-hidden');
+  })
+
+  let infoBtn = document.querySelector('.info__text-btn')
+  let secondParagraph = document.querySelector('.info__text-paragraph')
+  infoBtn.addEventListener('click', () => {
+    secondParagraph.classList.toggle('info__text-paragraph--hide');
+  })
+
+
+  let galleryImg = document.querySelectorAll('.gallery__item-img')
+
+window.addEventListener('resize', () => {
+  console.log('resize');
+  if(window.innerWidth <= 450) {
+
+  }
+})
+
+  if (window.innerWidth <= 450) {
+    galleryImg.forEach((element) => {
+      element.src = element.src.replace('.jpg', '-mobile.jpg')
+    })
+  }
 });
 
 
